c83c802753d688783edf3debc8493f1137126b24
Nicholas Williams <obinickenobi@Nicholass-MacBook-Pro.local>
2017-09-22 23:45:15 -0400

    major game update

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+var wins = 0;
+// var winsTotal = wins;
+var losses = 0;
+
+var currentPower = 0;
+
+/**Random Number**/
+var number = Math.floor((Math.random()* 100) + 19);
+
+/**Random Crystal Power**/
+var blue = Math.floor((Math.random()* 12) + 1);
+var red = Math.floor((Math.random()* 12) + 1);
+var yellow = Math.floor((Math.random()* 12) + 1);
+var green = Math.floor((Math.random()* 12) + 1);
+
+var configPower = function(){
+	$('#wins').empty();
+	$('#wins').append(wins);
+	$('#losses').empty();
+	$('#losses').append(losses);
+	$('.currentPower').empty();
+	$('.currentPower').append(currentPower);
+	
+}
+configPower();
+
+var reset = function(){
+	currentPower = 0;
+    number = Math.floor((Math.random()* 100) + 19);
+
+	$('.number').empty();
+	$('.number').append(number);
+
+	blue = Math.floor((Math.random()* 12) + 1);
+    red = Math.floor((Math.random()* 12) + 1);
+    yellow = Math.floor((Math.random()* 12) + 1);
+    green = Math.floor((Math.random()* 12) + 1);
+    configPower();
+}
+
+var config = function (){
+	if (currentPower == number) {
+		wins = wins + 1;
+		reset();
+}
+	else if(currentPower > number){
+		losses = losses + 1;
+		reset();
+}
+	else{
+		configPower();
+}}
+
+	$('.currentPower').append(currentPower);
+	$('.number').append(number);
+
+	$(document).ready(function(){
+	$('#blue').on("click", function(){
+		currentPower = currentPower + red;
+		config();
+	})
+	$('#red').on("click", function(){
+		currentPower  = currentPower  + blue;
+		config();
+	})
+	$('#yellow').on("click", function(){
+		currentPower  = currentPower  + yellow;
+		config();
+	})
+	$('#green').on("click", function(){
+		currentPower  = currentPower  + green;
+		config();
+	})
+});
+
